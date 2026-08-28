22c8d7867623bf46b6510176f296d09894ee46ec
RAVIBABU DODDA <113651922+ravibabu7844@users.noreply.github.com>
2026-08-29 00:41:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RIDEMOWA_CONFIG = {
+  GOOGLE_MAPS_API_KEY: "PASTE_RESTRICTED_BROWSER_KEY_HERE",
+  SUPABASE_URL: "https://eqreopqvemrpxeiwbost.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_PyzyCqh5lPb8HooEj54MfQ_Me7z2llY"
+};
